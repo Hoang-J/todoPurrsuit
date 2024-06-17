98777e3101c23911accbfef58c855fd400040455
COR Coding <corcoding@LAPTOP-N1N1CVP7.localdomain>
2024-06-17 09:19:10 -0400

    FP-3: Navbar updated to show only necessary links

--- capstone-project/src/components/Sidenav/Sidenavbar.js
+++ capstone-project/src/components/Sidenav/Sidenavbar.js
@@ -13,18 +13,24 @@ function Sidenavbar() {
     <div id='sidebar'>
     <Sidebar onBackdropClick={() => setToggled(false)} toggled={toggled} breakPoint="always"  backgroundColor='rgba(135, 207, 235, 0.852)'>
       <Menu>
-        <MenuItem component={<Link to="/todo" />}>Todo</MenuItem>
-        <MenuItem component={<Link to="/home" />}> Home </MenuItem>
-        <MenuItem component={<Link to="/registration" />}> Register </MenuItem>
-        <MenuItem component={<Link to="/login" />}> Login </MenuItem>
+        <br/>
+        <p/>
+        <br />
+        <MenuItem component={<Link to="/home" />}> Start Screen </MenuItem>
+        <p/>
+        {/* <MenuItem component={<Link to="/todo" />}>Todo</MenuItem> */}
+        {/* <MenuItem component={<Link to="/registration" />}> Register </MenuItem> */}
+        <MenuItem component={<Link to="/story1" />}> Game </MenuItem>
+        <p/>
         <MenuItem component={<Link to="/store" />}> Store </MenuItem>
+        <p/>
         {/* <MenuItem component={<Link to="/Level1" />}> Level 1 </MenuItem> */}
         {/* <MenuItem component={<Link to="/" />}> New Route</MenuItem> */}
-        <MenuItem component={<Link to="/story1" />}> Story1 </MenuItem>
-        <MenuItem component={<Link to="/story2" />}> Story2 </MenuItem>
+        {/* <MenuItem component={<Link to="/story2" />}> Story2 </MenuItem>
         <MenuItem component={<Link to="/story3" />}> Story3 </MenuItem>
         <MenuItem component={<Link to="/story4" />}> Story4 </MenuItem>
-        <MenuItem component={<Link to="/story5" />}> Story5 </MenuItem>
+        <MenuItem component={<Link to="/story5" />}> Story5 </MenuItem> */}
+        <MenuItem component={<Link to="/login" />}> Sign Out </MenuItem>
       </Menu>
     </Sidebar>
     <main style={{ display: 'flex', padding: 10 }}>
