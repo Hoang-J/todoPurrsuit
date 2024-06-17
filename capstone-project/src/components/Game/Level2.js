ea6e8813d4b042df536e9426fbd79c25ab350c5c
Nicolas Lozoya <nicolaslozoya23@yahoo.com>
2024-06-17 10:20:28 -0400

    added pause menu

--- capstone-project/src/components/Game/Level2.js
+++ capstone-project/src/components/Game/Level2.js
@@ -7,6 +7,10 @@ import Kitty from '../../images/catrunning.gif'
 import Doggy from '../../images/dog.gif'
 import Cloud from '../../images/thoughtcloud.png'
 import { useHistory } from 'react-router-dom';
+import Pausemenu from '../../images/pausemenu.png'
+import Close from '../../images/close.png'
+import Quit from '../../images/quit.png'
+import Sidenavbar from '../Sidenav/Sidenavbar'
 
 function Level2() {
   // Position state helps us move character on X axis
@@ -16,9 +20,8 @@ function Level2() {
   // jumping and jump height states helpe us initiate jumping animation and falling
   const [jumping, setJumping] = useState(false)
   const [jumpHeight, setJumpHeight] = useState(0)
-
   const [index, setIndex] = useState(0);
-  const [fading, setFading] = useState(false)
+  const [pause, setPause] = useState(false)
   const history = useHistory()
   const texts = [
     "Hello Kitty, you look lost",
@@ -54,6 +57,9 @@ function Level2() {
           }, 300) 
          }
       }
+      else if (e.key === 'Escape') {
+        setPause(true)
+      }
     };
     document.addEventListener('keydown', handleKeyPress);
   
@@ -62,30 +68,56 @@ function Level2() {
     };
   }, []);
   const freedom = () =>{
-    setFading(true)
     setTimeout(() => {
    history.push('/home')
     }, 500);
-    
+  }
+  const handleX = () => {
+    setPause(false)
   }
 
+  const quitter = () => {
+    history.push('/home')
+  }
   const transform = `translateX(${position}px) translateY(${jumpHeight}px) ${left ? 'scaleX(-1)' : ''}`;
   const transition = jumping ? 'transform 0.3s ease-out' : 'transform 0.3s ease-out'
  
   return (
     <>
+      <p id='esc'>Press Escape for pause menu</p>
     <div className='level2body'>
       <img id='Dungeon1' src={Dungeon1}></img> 
       <img id='Dungeon2' src={Dungeon2}></img>
     </div>
+    <div className='nav'>
+    <Sidenavbar />
+    </div>
+
     <img className='Cat2' style={{ transform, transition }} src={Kitty}></img>
     <img id='dog' src={Doggy}></img>
     <p id='dogtext'>{texts[index]}</p>
+     {/* our riddle, if you click on it your'e redirected towards the next level */}
     <div onClick={freedom} className='freedom'></div>
     <img id='cloud' src={Cloud}></img>
     {index < texts.length -1 && (
           <button id='button1' onClick={handleNextText}>Next</button>
         )}
+        {/*Render image if pause = true */}
+      {pause && (
+        <div className='Pmenu'>
+        <img src={Pausemenu} style={{
+          transform: `translateX(${position}px)`, transition
+        }} className='pauseMenu'></img>
+        {/* Close pausemenu */}
+        <img src={Close} style={{
+          transform: `translateX(${position}px)`, transition
+         }} onClick={handleX} id='X'></img>
+         {/* Quit game and go to home page */}
+         <img src={Quit} style={{
+          transform: `translateX(${position}px)`, transition
+        }} className='quit' onClick={quitter}></img> 
+        </div>
+      )}
       
     
     
--- capstone-project/src/components/Homepage/Home.js
+++ capstone-project/src/components/Homepage/Home.js
@@ -2,14 +2,23 @@
 import missingcat from '../../images/missingcat.png'
 import logo from '../../images/TodoPursuit.png'
 import './Home.css'
+import Play from '../../images/play.png'
+import { useHistory } from 'react-router-dom'
 function Home() {
+  const history = useHistory()
+  const play = () => {
+    history.push('/login')
+  }
   return (
     <>
     <div className='homeBody'>
       <img src={missingcat}  alt='Missingcat' id='cat'></img>
       <img src={logo} alt='logo' id='logo'></img>
       <img src='https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjFxdThhNGhpend3dDI3ODR4Y3NsNGRhZjB5dWpkM3V0cHZ4cmZxbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/C3ADmBThxaNdS/giphy.gif' id='catgif' alt='catgif'></img>
+      <div className='playScreen'></div>
+      <img onClick={play} id='play' src={Play}></img>
     </div>
+
     </>
   )
 }
